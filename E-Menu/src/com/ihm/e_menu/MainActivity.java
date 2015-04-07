fc1b345220a285352b0e6d02cad83ee83212f6ce
kranium632 <kranium@orange.fr>
2015-04-07 15:59:00 +0200

    Activity created

--- E-Menu/src/com/ihm/e_menu/MainActivity.java
+++ E-Menu/src/com/ihm/e_menu/MainActivity.java
@@ -2,11 +2,16 @@ package com.ihm.e_menu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends Activity {
 
+	public final static String LANGUAGE = "com.ihm.e_menu.LANGUAGE";
+	public final static String FR_LANGUAGE = "fr_FR";
+	public final static String EN_LANGUAGE = "en_UK";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,7 +26,15 @@ public class MainActivity extends Activity {
 	}
 	
 	public void showMenuFR(View view){
-		//Intent
+		Intent intent = new Intent(this, ShowMainMenu.class);
+		intent.putExtra(LANGUAGE, FR_LANGUAGE);
+		startActivity(intent);
+	}
+	
+	public void showMenuEN(View view){
+		Intent intent = new Intent(this, ShowMainMenu.class);
+		intent.putExtra(LANGUAGE, EN_LANGUAGE);
+		startActivity(intent);
 	}
 
 }
