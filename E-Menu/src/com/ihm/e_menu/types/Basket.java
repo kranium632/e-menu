d5acfb0de78a0378dd88920380f427faaa775312
kranium632 <kranium@orange.fr>
2015-04-27 18:19:25 +0200

    Added a "Basket" class to store the purchased items Created a "Carte" class (still has to be completed)

---
+++ E-Menu/src/com/ihm/e_menu/types/Basket.java
@@ -0,0 +1,76 @@
+package com.ihm.e_menu.types;
+
+import java.util.Vector;
+
+public class Basket {
+	private Vector<Plat> meals;
+	private Vector<Menu> menus;
+	private Vector<Boisson> drinks;
+	
+	public Basket(){
+		this.meals = new Vector<Plat>();
+		this.menus = new Vector<Menu>();
+		this.drinks = new Vector<Boisson>();
+	}
+	
+	public Basket(Vector<Plat> plats, Vector<Menu> menus, Vector<Boisson> drinks){
+		this.meals = plats;
+		this.menus = menus;
+		this.drinks = drinks;
+	}
+	
+	public void addPlat(Plat p){
+		this.meals.add(p);
+	}
+	
+	public void addMenu(Menu m){
+		this.menus.add(m);
+	}
+	
+	public void addBoisson(Boisson b){
+		this.drinks.add(b);
+	}
+	
+	public void removePlat(Plat p){
+		this.meals.remove(p);
+	}
+	
+	public void removeMenu(Menu m){
+		this.menus.remove(m);
+	}
+	
+	public void removeBoisson(Boisson b){
+		this.drinks.remove(b);
+	}
+	
+	public Vector<Plat> getMeals() {
+		return meals;
+	}
+
+	public Vector<Menu> getMenus() {
+		return menus;
+	}
+
+	public Vector<Boisson> getDrinks() {
+		return drinks;
+	}
+
+	public void setMeals(Vector<Plat> meals) {
+		this.meals = meals;
+	}
+
+	public void setMenus(Vector<Menu> menus) {
+		this.menus = menus;
+	}
+
+	public void setDrinks(Vector<Boisson> drinks) {
+		this.drinks = drinks;
+	}
+	
+	public void clearBasket(){
+		this.meals.clear();
+		this.menus.clear();
+		this.drinks.clear();
+	}
+
+}
