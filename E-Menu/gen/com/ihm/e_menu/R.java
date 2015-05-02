ba2784fda6ed148e4412861b598038d2539783bb
kranium632 <kranium@orange.fr>
2015-05-02 18:49:06 +0200

    Added the "Ajouter au panier" button, even if it does not match the agreed layout as test purpose, and the said button seems to work fine (not crash) Also added to the git the previous files that I forgot to add

--- E-Menu/gen/com/ihm/e_menu/R.java
+++ E-Menu/gen/com/ihm/e_menu/R.java
@@ -32,7 +32,8 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f08000d;
+        public static final int addToBasket=0x7f08000c;
         public static final int carteMenuLinearLayout=0x7f080000;
         public static final int mainMenuTableLayout1=0x7f080002;
         public static final int mainMenuTableRow1=0x7f080003;
@@ -77,8 +78,11 @@ public final class R {
         public static final int show_menus_as_pop_up=0x7f07000c;
     }
     public static final class string {
-        public static final int action_example=0x7f05002a;
+        public static final int action_example=0x7f05002d;
         public static final int action_settings=0x7f050005;
+        /**   
+         */
+        public static final int addToBasket=0x7f050023;
         /**   
          */
         public static final int app_name=0x7f050004;
@@ -86,24 +90,24 @@ public final class R {
         /**   
          */
         public static final int button_fr_FR=0x7f050006;
-        public static final int carte_desserts=0x7f05001e;
-        public static final int carte_drinks=0x7f05001f;
-        public static final int carte_meals=0x7f05001d;
+        public static final int carte_desserts=0x7f050020;
+        public static final int carte_drinks=0x7f050021;
+        public static final int carte_meals=0x7f05001f;
         /**  Carte 
          */
-        public static final int carte_starters=0x7f05001c;
-        public static final int carte_wines=0x7f050020;
+        public static final int carte_starters=0x7f05001e;
+        public static final int carte_wines=0x7f050022;
         /**   
          */
-        public static final int hello_world=0x7f050021;
+        public static final int hello_world=0x7f050024;
         /**   
          */
-        public static final int main_menu_carte=0x7f050009;
-        public static final int main_menu_drinks=0x7f05000b;
-        public static final int main_menu_enfants=0x7f05000d;
-        public static final int main_menu_menus=0x7f05000a;
-        public static final int main_menu_recherche=0x7f05000e;
-        public static final int main_menu_wines=0x7f05000c;
+        public static final int main_menu_carte=0x7f050011;
+        public static final int main_menu_drinks=0x7f050013;
+        public static final int main_menu_enfants=0x7f050015;
+        public static final int main_menu_menus=0x7f050012;
+        public static final int main_menu_recherche=0x7f050016;
+        public static final int main_menu_wines=0x7f050014;
         /**   
          */
         public static final int menu_1=0x7f050000;
@@ -111,35 +115,34 @@ public final class R {
         public static final int menu_3=0x7f050002;
         public static final int menu_4=0x7f050003;
         /**   
+  
          */
-        public static final int menu_carte=0x7f050015;
-        public static final int menu_children=0x7f050019;
-        public static final int menu_desserts=0x7f05001a;
-        public static final int menu_drinks=0x7f050017;
-        public static final int menu_menus=0x7f050016;
-        public static final int menu_search=0x7f05001b;
-        public static final int menu_wines=0x7f050018;
-        public static final int navigation_drawer_close=0x7f050029;
-        public static final int navigation_drawer_open=0x7f050028;
-        public static final int title_activity_carte=0x7f050023;
-        /**   
-         */
-        public static final int title_activity_menu_carte=0x7f05000f;
-        public static final int title_activity_menu_children=0x7f050011;
-        public static final int title_activity_menu_desserts=0x7f05002b;
-        public static final int title_activity_menu_drinks=0x7f050014;
-        public static final int title_activity_menu_menus=0x7f050010;
-        public static final int title_activity_menu_search=0x7f050012;
-        public static final int title_activity_menu_wines=0x7f050013;
-        public static final int title_activity_navigation=0x7f050024;
+        public static final int menu_carte=0x7f050017;
+        public static final int menu_children=0x7f05001b;
+        public static final int menu_desserts=0x7f05001c;
+        public static final int menu_drinks=0x7f050019;
+        public static final int menu_menus=0x7f050018;
+        public static final int menu_search=0x7f05001d;
+        public static final int menu_wines=0x7f05001a;
+        public static final int navigation_drawer_close=0x7f05002c;
+        public static final int navigation_drawer_open=0x7f05002b;
+        public static final int title_activity_carte=0x7f050026;
+        public static final int title_activity_menu_carte=0x7f050009;
+        public static final int title_activity_menu_children=0x7f05000b;
+        public static final int title_activity_menu_desserts=0x7f05000f;
+        public static final int title_activity_menu_drinks=0x7f05000e;
+        public static final int title_activity_menu_menus=0x7f05000a;
+        public static final int title_activity_menu_search=0x7f05000c;
+        public static final int title_activity_menu_wines=0x7f05000d;
+        public static final int title_activity_navigation=0x7f050027;
         /**   
          */
         public static final int title_activity_show_main_menu=0x7f050008;
-        public static final int title_activity_show_menus_as_pop_up=0x7f05002c;
-        public static final int title_section1=0x7f050025;
-        public static final int title_section2=0x7f050026;
-        public static final int title_section3=0x7f050027;
-        public static final int welcome=0x7f050022;
+        public static final int title_activity_show_menus_as_pop_up=0x7f050010;
+        public static final int title_section1=0x7f050028;
+        public static final int title_section2=0x7f050029;
+        public static final int title_section3=0x7f05002a;
+        public static final int welcome=0x7f050025;
     }
     public static final class style {
         /** 
---
+++ E-Menu/src/com/ihm/e_menu/ShowMenusAsPopUp.java
@@ -0,0 +1,60 @@
+package com.ihm.e_menu;
+
+import java.util.Vector;
+
+import com.ihm.e_menu.types.Boisson;
+import com.ihm.e_menu.types.CompleteList;
+import com.ihm.e_menu.types.GlobalBasket;
+import com.ihm.e_menu.types.MenuA;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ShowMenusAsPopUp extends Activity {
+	public MenuA m = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_show_menus_as_pop_up);
+
+		Bundle extras = getIntent().getExtras();
+		int id = extras.getInt(MenuMenus.MENU_ID);
+
+		this.m = CompleteList.getMenus(id);
+		final TextView textViewToChange = (TextView)findViewById(R.id.tv);
+		if (this.m != null){
+			String s = this.m.getName() + "\n" + this.m.getDescription() + "\n";
+			Vector<Boisson> b = this.m.getBoissons();
+			if (b.size() > 0)
+				s += "Liste des boissons comprise dans le menu :\n";
+			for (int i = 0; i < b.size(); i++){
+				s += "- " + b.get(i).getName() + "\n";
+			}
+
+			textViewToChange.setText(s);
+		}
+		else {
+			textViewToChange.setText("Une erreur est survenue, veuillez contacter le personnel du restaurant.");
+			final Button buttonToChange = (Button)findViewById(R.id.addToBasket);
+			buttonToChange.setVisibility(View.INVISIBLE);
+		}
+
+	}
+
+	public void addToBasket(View v){
+		if (this.m != null)
+			GlobalBasket.addMenu(m);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.show_menus_as_pop_up, menu);
+		return true;
+	}
+
+}
